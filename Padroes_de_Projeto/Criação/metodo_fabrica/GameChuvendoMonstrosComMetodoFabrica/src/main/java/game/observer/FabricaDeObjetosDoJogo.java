d03204bcd61971d6b7f3510ea3b582e2e80c84ee
felipefo <felipefo@gmail.com>
2017-02-14 09:09:37 -0200

    .
    .

---
+++ Padroes_de_Projeto/Criação/metodo_fabrica/GameChuvendoMonstrosComMetodoFabrica/src/main/java/game/observer/FabricaDeObjetosDoJogo.java
@@ -0,0 +1,58 @@
+package game.observer;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.newdawn.slick.Image;
+
+/**
+ *
+ * @author felipe
+ */
+public class FabricaDeObjetosDoJogo {
+
+    String filePath;
+
+    FabricaDeObjetosDoJogo() {
+
+        try {
+            File file = new File(".");
+            filePath = file.getCanonicalPath();
+        } catch (IOException ex) {
+            Logger.getLogger(FabricaDeObjetosDoJogo.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public Image criarImagensNotificacao(String tipo) {        
+        //gameover
+        //ganhou   
+        return null;
+    }
+
+    public Inimigo criarInimigo(int tipoInimigo) {
+        Inimigo inimigo = null;
+        if (tipoInimigo == 1) {
+            inimigo = new Inimigo(filePath + "\\src\\main\\java\\devil1.png", 500, -100);
+        } else if (tipoInimigo == 2) {
+            inimigo = new Inimigo(filePath + "\\src\\main\\java\\devil2.png", 300, -00);
+        } else if (tipoInimigo == 3) {
+            inimigo = new Inimigo(filePath + "\\src\\main\\java\\devil3.png", 400, -250);
+        }
+        else if (tipoInimigo == 4) {
+          inimigo = new InimigoEsperto(filePath + "\\src\\main\\java\\devil3.png", 200, -250);
+
+        }
+        return inimigo;
+    }
+
+    public void criarLand(int tipoLand) {
+        //planodefundo
+
+    }
+
+    public void criarHerois(int tipoHeroi) {
+        //heroi
+    }
+
+}
