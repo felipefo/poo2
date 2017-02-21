f0b62dce760d97d01fabce0368434f44e5bd066d
felipefo <felipefo@users.noreply.github.com>
2017-02-21 10:08:17 -0300

    Update Edificio.java

--- RevisaoJava/Edificio.java
+++ RevisaoJava/Edificio.java
@@ -2,6 +2,6 @@ import java.util.ArrayList;
 
 public class Edificio{
 
-  private ArrayList<Andares> andares = new ArrayList<Andares>();
+  private ArrayList<Andar> andares = new ArrayList<Andar>();
   
 }
