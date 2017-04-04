288423159574853d81a5e299d1790d029d4f6da7
felipefo <felipefo@gmail.com>
2017-04-04 17:26:06 -0300

    .
    .

---
+++ Padroes_de_Projeto/Estrutural/Decorator/JavaWindowDecorator/src/decorator/BorderDecorator.java
@@ -0,0 +1,24 @@
+package decorator;
+
+import javax.swing.JComponent;
+import java.awt.Graphics;
+import java.awt.BorderLayout;
+
+public class BorderDecorator extends DecoratorAbstrato{
+  
+// decorated component  
+public BorderDecorator(JComponent component) {
+    child = component;
+    this.setLayout(new BorderLayout());
+    this.add(child);
+  }
+
+  public void paint(Graphics g) {
+    super.paint(g);
+    int height = this.getHeight();
+    int width = this.getWidth();
+    g.drawRect(0, 0, width-1 , height-1 );
+  }
+   
+
+}
---
+++ Padroes_de_Projeto/Estrutural/Decorator/JavaWindowDecorator/src/decorator/DecoratorAbstrato.java
@@ -0,0 +1,11 @@
+
+package decorator;
+
+import javax.swing.JComponent;
+
+
+public abstract class   DecoratorAbstrato extends JComponent {    
+    protected JComponent child; 
+
+    
+}
