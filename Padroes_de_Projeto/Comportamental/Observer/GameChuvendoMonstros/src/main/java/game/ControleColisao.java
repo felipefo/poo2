ad5b0f1720121b6332544074bd1fdb1040e52ec9
felipefo <felipefo@gmail.com>
2017-05-10 08:51:49 -0300

    .
    .

--- Padroes_de_Projeto/Comportamental/Observer/GameChuvendoMonstros/src/main/java/game/ControleColisao.java
+++ Padroes_de_Projeto/Comportamental/Observer/GameChuvendoMonstros/src/main/java/game/ControleColisao.java
@@ -21,9 +21,9 @@ public class ControleColisao  extends Colisao{
             this.zumbi = (Inimigo) ob;                 
         if (this.heroi != null && this.zumbi != null) {
             Rectangle first = new Rectangle(heroi.getPosX(), heroi.getPosY(),
-                    heroi.getImage().getWidth() - 20, heroi.getImage().getHeight() - 20);
+                    heroi.getImage().getWidth() - 25, heroi.getImage().getHeight() - 25);
             Rectangle second = new Rectangle(zumbi.getPosX(), zumbi.getPosY(),
-                    zumbi.getImage().getWidth() - 20, zumbi.getImage().getHeight() - 20);
+                    zumbi.getImage().getWidth() - 25, zumbi.getImage().getHeight() - 25);
             if (first.intersects(second)) {
                 controller.pause();
             }
