a15d15c461fb189a4648a7e3778d2ab19386048d
felipefo <felipefo@gmail.com>
2017-05-24 08:01:04 -0300

    .
    .

--- Padroes_de_Projeto/Comportamental/Comando/CommandPattern/src/command/pattern/CommandPattern.java
+++ Padroes_de_Projeto/Comportamental/Comando/CommandPattern/src/command/pattern/CommandPattern.java
@@ -1,21 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package command.pattern;
 
-/**
- *
- * @author felipe
- */
 public class CommandPattern {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+         TelaCommand tela = new TelaCommand();
+        tela.setVisible(true);
     }
     
 }
