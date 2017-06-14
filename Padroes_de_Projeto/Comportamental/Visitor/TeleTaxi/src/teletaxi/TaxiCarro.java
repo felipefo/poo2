dcaedbaf45c23082207c75dcf219404c1b0141cd
felipefo <felipefo@gmail.com>
2017-06-14 08:34:45 -0300

    .
    .

--- Padroes_de_Projeto/Comportamental/Visitor/TeleTaxi/src/teletaxi/TaxiCarro.java
+++ Padroes_de_Projeto/Comportamental/Visitor/TeleTaxi/src/teletaxi/TaxiCarro.java
@@ -1,31 +1,19 @@
 
 package teletaxi;
-
 public class TaxiCarro extends Taxi {
-
     @Override
-    public void parar() {  
-       System.out.println("Apertar o freio");
-    }
-
+    public void parar(){System.out.println("Apertar o freio");}
     @Override
-    public void andarParaFrente() {        
-        System.out.println("Acelerar");        
-    }
-
+    public void andarParaFrente() {System.out.println("Acelerar");}
     @Override
     public void virarEsquerda() {        
         System.out.println("Dar a seta");
-        System.out.println("Virar voltante para direira");
-
-    }
-
+        System.out.println("Virar voltante para direira");}
     @Override
     public void virarDireita() {
         System.out.println("Dar a seta");
         System.out.println("Virar voltante para direira");
     }
-
     @Override
     public void andarParaTras() {
         System.out.println("Acelerar contrario");        
