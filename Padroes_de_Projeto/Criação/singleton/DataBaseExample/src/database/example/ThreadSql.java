d40aae201a01c98cc70abc433afc9184e7558401
felipefo <felipefo@users.noreply.github.com>
2018-04-15 12:58:24 -0300

    .
    .

--- Padroes_de_Projeto/Criação/singleton/DataBaseExample/src/database/example/ThreadSql.java
+++ Padroes_de_Projeto/Criação/singleton/DataBaseExample/src/database/example/ThreadSql.java
@@ -1,17 +1,12 @@
 package database.example;
 
-
 public class ThreadSql implements Runnable
 {
    public void run()
-   {
-      
+   {      
       InsertRecord insert = new InsertRecord();
       for(int i=0;i< 100;i++)
         insert.insertRecords();            
-      
-     // UpdatetRecord update = new UpdatetRecord();
-     // update.updateRecord();      
       SelectTable select = new SelectTable();
       select.selectTable();                  
       DeleteRecord delete = new DeleteRecord();
