58021d77f4092ec3de106a437c89824362cdc623
felipefo <felipefo@users.noreply.github.com>
2018-04-24 13:53:23 -0300

    .
    .

---
+++ Padroes_de_Projeto/Estrutural/Adapter/AdapterCliente/src/adaptercliente/model/Cliente.java
@@ -0,0 +1,46 @@
+
+package adaptercliente.model;
+
+
+public class Cliente {
+    
+    private String nomeCompleto;
+    private String telefone;    
+    private String email;
+    private String celular;
+
+    public String getNomeCompleto() {
+        return nomeCompleto;
+    }
+
+    public void setNomeCompleto(String nomeCompleto) {
+        this.nomeCompleto = nomeCompleto;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+    
+    
+    
+}
