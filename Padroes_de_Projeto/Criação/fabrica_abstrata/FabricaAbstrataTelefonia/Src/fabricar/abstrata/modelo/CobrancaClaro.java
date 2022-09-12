6e1cbe9cfff17e7e9a1ece3af41ee38aeedcd1f5
JhovanyMurgia <98408956+JhovanyMurgia@users.noreply.github.com>
2022-09-12 17:02:35 -0300

    Create CobrancaClaro.java

---
+++ Padroes_de_Projeto/Criação/fabrica_abstrata/FabricaAbstrataTelefonia/Src/fabricar/abstrata/modelo/CobrancaClaro.java
@@ -0,0 +1,11 @@
+package fabrica.abstrata.modelo;
+
+import fabrica.abstrata.modelo.Cobranca;
+
+public class CobrancaClaro implements Cobranca {
+
+    @Override
+    public void cobrar(int telefone) {
+        System.out.println("Cobrar via claro");
+    }
+}
